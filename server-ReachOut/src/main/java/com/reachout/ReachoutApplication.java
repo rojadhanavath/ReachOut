dc927167063c5895e6ecbd3e1bfe432fbd84aad7
rojadhanavath <roja.d293@gmail.com>
2018-07-14 18:26:32 -0400

    Server bootstrap

---
+++ server-ReachOut/src/main/java/com/reachout/ReachoutApplication.java
@@ -0,0 +1,12 @@
+package com.reachout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReachoutApplication.class, args);
+	}
+}
